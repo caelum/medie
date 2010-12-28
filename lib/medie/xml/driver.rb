2feae14fc1626ed614e64c406380b0044edf0632
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2010-12-28 20:27:12 -0200

    updating the xml driver accepted media types

--- lib/medie/xml/driver.rb
+++ lib/medie/xml/driver.rb
@@ -25,8 +25,12 @@ module Medie
       end
 
       def can_handle?(content_type)
-        !(content_type.nil? || content_type.split(";")[0]!="application/xml")
+        !(content_type.nil? ||
+          content_type.split(";")[0]!="application/xml" ||
+          content_type.split(";")[0]!="application/atom+xml" ||
+          content_type.split(";")[0]!="text/xml")
       end
+
     end
   end
 end
