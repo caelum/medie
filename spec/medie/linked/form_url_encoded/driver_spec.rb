c2e52bc5a4e733cd6807f7c6d5dab7f186f76cbc
Douglas Campos <qmx@qmx.me>
2010-12-28 20:58:49 -0200

    tests should be order independent

--- spec/medie/linked/form_url_encoded/driver_spec.rb
+++ spec/medie/linked/form_url_encoded/driver_spec.rb
@@ -2,15 +2,18 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
 
 describe Medie::FormUrlEncoded::Driver do
   context "when unmarshalling" do
-    
+
     it "when passing not a hash, returns itself" do
-    	Medie::FormUrlEncoded::Driver.marshal("some content", "").should == "some content"
+      Medie::FormUrlEncoded::Driver.marshal("some content", "").should == "some content"
     end
-    
+
     it "when passing a hash should concatenate" do
       params = {:user => "guilherme", :password => "my_pass"}
-    	Medie::FormUrlEncoded::Driver.marshal(params, "").should == "password=my_pass&user=guilherme"
+      marshalled_params = Medie::FormUrlEncoded::Driver.marshal(params, "")
+      marshalled_params.should =~ /password=my_pass/
+      marshalled_params.should =~ /user=guilherme/
+      marshalled_params.should =~ /&/
     end
-    
+
   end
 end
