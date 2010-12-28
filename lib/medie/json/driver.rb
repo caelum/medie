5add0fd603172d8d57ad10356b85f397087f87ae
Douglas Campos <qmx@qmx.me>
2010-12-28 20:05:55 -0200

    matching method order of Xml::Driver

--- lib/medie/json/driver.rb
+++ lib/medie/json/driver.rb
@@ -3,15 +3,6 @@ module Medie
     # Receives Json representations and turn them into hashes. Turn
     # hashes into json objects.
     class Driver
-
-      def unmarshal(content)
-        if content.nil?
-          return {}.extend(Methodize).extend(Linked)
-        end
-    
-        ::JSON.parse(content).extend(Methodize).extend(Linked)
-      end
-
       def marshal(obj, rel)
         if obj.kind_of? String
           obj
@@ -20,6 +11,14 @@ module Medie
         end
       end
   
+      def unmarshal(content)
+        if content.nil?
+          return {}.extend(Methodize).extend(Linked)
+        end
+    
+        ::JSON.parse(content).extend(Methodize).extend(Linked)
+      end
+
       def can_handle?(content_type)
         !(content_type.nil? || content_type.split(";")[0]!="application/json")
       end
